328026929207a3945c2ede31c8514e15bf848fe7
Lucas Pedro da Hora <lucaspedro517@gmail.com>
2025-04-28 18:27:01 -0300

    🐛Bug: Fixing error to build not-found page.

--- apps/web/src/app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex items-center justify-center w-full h-full text-2xl font-bold text-center text-gray-500">
-      <h1>404 - Page Not Found</h1>
-    </div>
-  );
-}
